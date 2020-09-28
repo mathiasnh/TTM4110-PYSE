68fdf4f221fed042c808aa6a5411843c68bbd4e4
Mathias Henriksen <mnh@vitalthings.com>
2020-09-28 16:27:00 +0200

    WIP: 2b

--- lab2/2b.py
+++ lab2/2b.py
@@ -4,7 +4,11 @@ import matplotlib.pyplot as plt
 import math
 
 T_guard = 60
+T_LANDING = 60
+T_TAKEOFF = 60
 P_DELAY = 0.5
+MU_DELAY = 0
+MU_TURNAROUND = 45
 SIM_TIME = 86400
 
 def get_scheduled_time(time):
@@ -25,30 +29,69 @@ def is_delayed():
     return np.random.choice([True, False], p=[P_DELAY, 1 - P_DELAY])
 
 def get_delayed_time():
-    return np.random.gamma(3, 0)
+    return np.random.gamma(3, MU_DELAY)
 
-def take_means(planes):
-    prev = 0
-    means = []
-    IAs = []
+def get_turnaround_time():
+    return np.random.gamma(7, MU_TURNAROUND)
+
+def take_means(planes, what):
+    prev = 5
+    means = [0,0,0,0,0]
+    thing = []
     for plane in planes:
         hour = math.floor(plane.scheduled)
+        print(hour)
         if hour == prev:
-            IAs.append(plane.inter_arrival)
+            if what == "landing":
+                thing.append(plane.landing_q_time)
+            elif what == "takeoff":
+                thing.append(plane.takeoff_q_time)
         else:
-            means.append(sum(IAs)/len(IAs))
-            IAs = [plane.inter_arrival]
+            means.append(sum(thing)/len(thing))
+            if what == "landing":
+                thing = [plane.landing_q_time]
+            elif what == "takeoff":
+                thing = [plane.takeoff_q_time]
+            
             prev = hour
+    means.append(sum(thing)/len(thing))
     return means
 
 class Plane:
-    def __init__(self, env, scheduled, inter_arrival, runways):
+    def __init__(self, env, scheduled, inter_arrival, delay, runways):
         self.scheduled = scheduled
-        self.inter_arrival = inter_arrival
-        #env.process(self.land())
+        self.inter_arrival = inter_arrival + delay
+        self.delay = delay
+        self.env = env
+        self.landing_q_time = 0
+        self.takeoff_q_time = 0
+        env.process(self.land())
 
     def land(self):
-        pass
+        yield self.env.timeout(self.delay)
+
+        landing_start = self.env.now
+
+        with runways.request(priority=1) as req:
+            yield req
+            yield self.env.timeout(T_LANDING)
+
+        landing_end = self.env.now
+        
+        yield self.env.timeout(get_turnaround_time())
+
+        takeoff_start = self.env.now
+
+        with runways.request(priority=2) as req:
+            yield req
+            yield self.env.timeout(T_TAKEOFF)
+        
+        takeoff_end = self.env.now
+
+        self.landing_q_time = (landing_end - landing_start) - T_LANDING
+        self.takeoff_q_time = (takeoff_end - takeoff_start) - T_TAKEOFF
+
+        #print(landing_q_time, takeoff_q_time)
 
 
 class PlaneGenerator:
@@ -66,13 +109,13 @@ class PlaneGenerator:
 
             if T is not None:
                 inter_arrival = np.maximum(T_guard, T)
-                self.planes.append(Plane(env, t/3600, inter_arrival + delay, runways))
                 
                 if is_delayed():
                     delay = get_delayed_time()
                 else:
                     delay = 0
 
+                self.planes.append(Plane(env, t/3600, inter_arrival, delay, runways))
                 # TODO: Schedule plane
 
                 yield self.env.timeout(inter_arrival)
@@ -82,18 +125,21 @@ class PlaneGenerator:
 
 env = simpy.Environment()
 
-runways = [simpy.PriorityResource(env), simpy.PriorityResource(env)]
+runways = simpy.PriorityResource(env, capacity=2)
 gen = PlaneGenerator(env, runways)
 
 
 env.run(until=SIM_TIME)
 
-means = take_means(gen.planes)
+landing_means = take_means(gen.planes, "landing")
+takeoff_means = take_means(gen.planes, "takeoff")
 
-plt.plot([i for i in range(len(means))], means)
+plt.plot([i for i in range(len(landing_means))], landing_means)
+plt.plot([i for i in range(len(takeoff_means))], takeoff_means)
+plt.legend(['Landing', 'take-off'])
 plt.xlabel('Hour')
-plt.ylabel('Inter-arrival time')
-plt.title('Mean inter-arrival time per hour (µ_delay=500)', fontsize=16)
+plt.ylabel('Qeueu time (seconds)')
+plt.title('Mean landing and take-off times', fontsize=16)
 
 plt.show()
 
